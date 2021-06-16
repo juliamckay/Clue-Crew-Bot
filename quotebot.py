5872277a6e2f5e6dfe98a71c073b392d0c6ea2fa
Julia McKay <juliamckay@ufl.edu>
2021-06-16 18:51:27 -0400

    removed image upload, switched to twitter link

--- quotebot.py
+++ quotebot.py
@@ -4,6 +4,10 @@ import time
 import random
 import json
 from os import environ
+#import config
+
+import config
+
 
 def create_api(consumer_key, consumer_secret_key, access_token, access_token_secret):
     # Authenticate to Twitter
@@ -18,24 +22,15 @@ def get_random_tweet(tweets):
     random_tweet = random.choice(tweets)
     return random_tweet
 
-def create_tweet(tweets, tweeted_quotes, api):
-    #choose random tweet, check that it has not been tweeted yet
-    while True:
-        tweet = get_random_tweet(tweets)
-        if not tweet in tweeted_quotes:
-            break
+def create_tweet(tweets, api):
+    #choose random tweet
+    tweet = get_random_tweet(tweets)
+
     #post tweet
     message = ''
-    media_list = []
     if "status" in tweet:
         message = tweet["status"]
-    if "image" in tweet:
-        filename = "images/" + tweet["image"]
-        image = api.media_upload(filename)
-        media_list.append(image.media_id)
-    api.update_status(status=message, media_ids=media_list)
-    #add to already tweeted
-    tweeted_quotes.append(tweet)
+    api.update_status(status=message)
 
 
 if __name__ == "__main__":
@@ -43,28 +38,28 @@ if __name__ == "__main__":
     consumer_secret_key = environ['CONSUMER_SECRET']
     access_token = environ['ACCESS_KEY']
     access_token_secret = environ['ACCESS_SECRET']
+
+    #consumer_key = config.api_key
+    #consumer_secret_key = config.api_secret
+    #access_token = config.access_token
+    #access_token_secret = config.access_secret
+
     api = create_api(consumer_key, consumer_secret_key, access_token, access_token_secret)
     tweet_file = 'tweets.json'
-    prev_tweets_file = 'prev_tweeted.json'
 
     with open(tweet_file, 'r') as f:
         tweets = json.load(f)
 
-    with open(prev_tweets_file, 'r') as f:
-        previously_tweeted = json.load(f)
-
     # Tweet quote
-    interval = 60 * 60 * 8  # three times a day
-    while True:
-        # if we've tweeted all the quotes in the list reset
-        if len(tweets) == len(previously_tweeted):
-            previously_tweeted = []
-            #print("reset list")
-            with open(prev_tweets_file, 'w') as f:
-                json.dump(previously_tweeted, f)
-        create_tweet(tweets, previously_tweeted, api)
-        #update previously tweeted file
-        with open(prev_tweets_file, 'w') as f:
-            json.dump(previously_tweeted, f)
-        print(previously_tweeted)
-        time.sleep(interval)
+    tweet_created = False
+    while not tweet_created:
+        #try to create tweet, expect duplicate tweet error and pass if raised
+        try:
+            create_tweet(tweets, api)
+            tweet_created = True
+        except tweepy.TweepError as error:
+            if error.api_code == 187:
+                print("duplicate message error")
+            else:
+                raise error
+
